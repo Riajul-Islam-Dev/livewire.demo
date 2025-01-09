05a4a6139f55f163c61789f63d1936ca3d3ee516
Riajul Islam <ritewu2014@gmail.com>
2025-01-10 03:07:15 +0600

    gpoc & /oc added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/oc', function () {
+    Artisan::call('optimize:clear');
+    return '<pre>' . Artisan::output() . '</pre>';
 });
